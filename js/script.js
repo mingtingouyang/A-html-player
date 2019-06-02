3725afa0333cf4b850d3449f47f79f662f120078
mingtingouyang <36928035+mingtingouyang@users.noreply.github.com>
2019-06-02 09:22:08 +0800

    javascript update

--- js/script.js
+++ js/script.js
@@ -39,25 +39,24 @@ cursor.onmousedown = function () {
 	playButtom.style.backgroundImage = 'url("img/playOn.png")'
 	tmp = 0
 	tmp2 = 1
-	back.onmousemove = function (event) {
-		if(event.pageX <= process.offsetLeft){
-			video.currentTime = 0
-		}else if(event.pageX >= (process.offsetLeft + process.clientWidth)){
-			video.currentTime = video.duration
-		}else{
-			video.currentTime = video.duration * ((event.pageX - process.offsetLeft) / process.clientWidth)
-		}
-	}
 	cursor.style.boxShadow = '0 0 7px red'
 	line.style.boxShadow = '0 0 7px red'
+}
+back.onmousemove = function (event) {
+	if(event.pageX <= process.offsetLeft && tmp2 == 1){
+		video.currentTime = 0
+	}else if(event.pageX >= (process.offsetLeft + process.clientWidth) && tmp2 == 1){
+		video.currentTime = video.duration
+	}else if(tmp2 == 1){
+		video.currentTime = video.duration * ((event.pageX - process.offsetLeft) / process.clientWidth)
+	}
 }
 back.onmouseup = function () {
 	if(tmp2 == 1){
 		video.play()
 		tmp = 1
 		playButtom.style.backgroundImage = 'url("img/pauseOn.png")'
-		tmp2 == 0
-		back.onmousemove = null
+		tmp2 = 0
 	}
 	cursor.style.boxShadow = null
 	line.style.boxShadow = null
